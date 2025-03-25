e5c7a20a6eca06ab740568a90b80c4422674fe09
Rabia27-bit <rabiatunio.bscsf22@iba-suk.edu.pk>
2025-03-25 09:42:10 +0500

    Timer

--- ead-midexam/src/CountdownLightSwitch.jsx
+++ ead-midexam/src/CountdownLightSwitch.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './index.css'
 
 function CountdownLightSwitch() {
+    const [timer,setTimer]=useState(0)
+
+    function handleTimer(){
+        setInterval(()=>{
+            setTimer(timer+1)
+        },1000)
+    }
+
   return (
     <div className="container">
     <div className="header">
       <h1>Countdown & Light Switch</h1>
       <div className="toggle-container">
         <label className="toggle-switch">
-          <input type="checkbox" id="themeToggle" />
+          <input type="checkbox" id="themeToggle"/>
           <span className="slider"></span>
         </label>
         <span>Light Mode</span>
@@ -19,9 +27,9 @@ function CountdownLightSwitch() {
       <div className="progress-bar">
         <div className="progress" id="progress"></div>
       </div>
-      <div className="timer" id="timerDisplay">30s</div>
+      <div className="timer" id="timerDisplay">{timer}</div>
       <div className="btn-group">
-        <button id="startButton">Start Timer</button>
+        <button id="startButton" onClick={handleTimer}>Start Timer</button>
         <button id="resetButton">Reset Timer</button>  
         {/* display none */}
       </div>
